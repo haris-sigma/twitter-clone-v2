0d40a079cab9ff9805cc6f871c3b933d6e9f32b2
Haris Samingan <muhamad_haris@hotmail.com>
2023-07-08 19:58:57 +0800

    add

--- src/components/ProfilePostCard.jsx
+++ src/components/ProfilePostCard.jsx
@@ -1,18 +1,18 @@
-import axios from "axios";
-import jwtDecode from "jwt-decode";
 import { useEffect, useState } from "react";
 import { Button, Col, Image, Row } from "react-bootstrap";
+import jwt_decode from "jwt-decode";
+import axios from "axios";
 
 export default function ProfilePostCard({ content, postId }) {
   const [likes, setLikes] = useState([]);
 
+  // Decoding to get the userId
   const token = localStorage.getItem("authToken");
-  const decode = jwtDecode(token);
+  const decode = jwt_decode(token);
   const userId = decode.id;
 
   const pic =
     "https://pbs.twimg.com/profile_images/1587405892437221376/h167Jlb2_400x400.jpg";
-
   const BASE_URL =
     "https://twitter-api-sigmaschooltech.sigma-school-full-stack.repl.co";
 
@@ -20,22 +20,33 @@ export default function ProfilePostCard({ content, postId }) {
     fetch(`${BASE_URL}/likes/post/${postId}`)
       .then((response) => response.json())
       .then((data) => setLikes(data))
-      .catch((error) => console.error("error", error));
+      .catch((error) => console.error("Error:", error));
   }, [postId]);
 
   const isLiked = likes.some((like) => like.user_id === userId);
 
-  const handleLike = () => (isLiked ? removeFromLikes() : addLikes());
+  const handleLike = () => (isLiked ? removeFromLikes() : addToLikes());
 
-  const addLikes = () => {
-    setLikes([...likes, { user_id: userId }]);
-    axios.post(`${BASE_URL}/likes`, { user_id: userId, post_id: postId });
+  const addToLikes = () => {
+    axios
+      .post(`${BASE_URL}/likes`, {
+        user_id: userId,
+        post_id: postId,
+      })
+      .then((response) => {
+        setLikes([...likes, { ...response.data, likes_id: response.data.id }]);
+      })
+      .catch((error) => console.error("Error:", error));
   };
+
   const removeFromLikes = () => {
     const like = likes.find((like) => like.user_id === userId);
+    console.log(like);
     if (like) {
-      axios.delete(`${BASE_URL}/likes/${like.likes_id}`);
-      setLikes(likes.filter((like) => like.user_id !== userId));
+      axios
+        .put(`${BASE_URL}/likes/${like.likes_id}`)
+        .then(() => setLikes(likes.filter((like) => like.user_id !== userId)))
+        .catch((error) => console.error("Error:", error));
     }
   };
 
@@ -52,9 +63,9 @@ export default function ProfilePostCard({ content, postId }) {
       </Col>
       <Col>
         <strong>Haris</strong>
-        <span>@haris.samingan . Apr 16</span>
+        <span> @haris.samingnan · Apr 16</span>
         <p>{content}</p>
-        <div className="d-flex justify-content-between align-items-center">
+        <div className="d-flex justify-content-between">
           <Button variant="light">
             <i className="bi bi-chat"></i>
           </Button>
@@ -62,11 +73,15 @@ export default function ProfilePostCard({ content, postId }) {
             <i className="bi bi-repeat"></i>
           </Button>
           <Button variant="light" onClick={handleLike}>
-            {isLiked && <i className="bi bi-heart-fill text-danger"></i>}
-            {!isLiked && <i className="bi bi-heart"></i>}
+            {isLiked ? (
+              <i className="bi bi-heart-fill text-danger"></i>
+            ) : (
+              <i className="bi bi-heart"></i>
+            )}
+            {likes.length}
           </Button>
           <Button variant="light">
-            <i className="bi bi-graph-up"></i>
+            <i className="bi bi-graph-up"></i> 61
           </Button>
           <Button variant="light">
             <i className="bi bi-upload"></i>
